d402cc9e4068d77e050a43eadb063910e8c81e97
Marc Foley <m.foley.88@gmail.com>
2021-10-19 10:33:10 +0100

    use glyphsLib

--- bumpfontversion/glyphshandler.py
+++ bumpfontversion/glyphshandler.py
@@ -1,7 +1,4 @@
-import openstep_plist
-# Using this instead of glyphslib gives us v2 and v2 compatiblity without
-# faffing around
-
+from glyphsLib import GSFont
 import logging
 from bumpversion.version_part import Version, VersionPart
 
@@ -13,20 +10,17 @@ class GlyphsHandler:
         return file.endswith(".glyphs")
 
     def current_version(self, file):
-        with open(file, "r", encoding="utf-8") as fp:
-            font = openstep_plist.load(fp, use_numbers=True)
+        font = GSFont(file)
         return Version(
             {
-                "major": VersionPart(str(font["versionMajor"] or 0)),
-                "minor": VersionPart(str(font["versionMinor"] or 0)),
+                "major": VersionPart(str(font.versionMajor or 0)),
+                "minor": VersionPart(str(font.versionMinor or 0)),
             }
         )
 
     def set_version(self, file, new_version):
-        with open(file, "r", encoding="utf-8") as fp:
-            font = openstep_plist.load(fp, use_numbers=True)
-        font["versionMajor"] = int(new_version["major"].value)
-        font["versionMinor"] = int(new_version["minor"].value)
+        font = GSFont(file)
+        font.versionMajor = int(new_version["major"].value)
+        font.versionMinor = int(new_version["minor"].value)
         logger.info(f"Saving file {file}")
-        with open(file, "w", encoding="utf-8") as fp:
-            openstep_plist.dump(font, fp)
+        font.save(file)
